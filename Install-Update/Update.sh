ac1d60fd09f4a61e558d9c846e6d9c3586245f26
By Speece <72341542+by-speece@users.noreply.github.com>
2021-01-02 15:56:40 +0100

    Create Update.sh

---
+++ Install-Update/Update.sh
@@ -0,0 +1,20 @@
+echo Pobieranie Aktualizacji
+git pull 
+cd ~/.settings/TheSamePackages
+echo Uaktualnianie Pakietów
+sh start.sh
+cd ~/.settings/Install-Update
+echo Czyszczenie systemu
+sh clean.sh
+echo Uaktualnianie Plików Statycznych
+rm -rf ~/.bashrc
+cp -rf ~/.settings/.bashrc 	~/
+rm -rf ~/.nanorc
+cp -rf ~/.settings/.nanorc	~/
+rm -rf ~/.p10k.zsh
+cp -rf ~/.settings/.p10k.zsh	~/
+rm -rf ~/.zshrc
+cp -rf ~/.settings/.zshrc	~/
+rm -rf ~/.zshrc.zni
+cp -rf ~/.settings/.zshrc.zni	~/
+echo Całe DE zostało uaktualnione
